13d8dc02fcc3b3e0af4e9a7ed60425ab4d75921f
Jude Jonathan <jonathanjude27@gmail.com>
2019-10-02 22:25:21 +0100

    Handled CORS

---
+++ config/cors.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'supportsCrdentials' => false,
+    'allowedOrigins' => ['*'],
+    'allowedOriginPatterns' => [],
+    'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
+    'allowedMethods' => ['*'],
+    'exposedMethods' => [],
+    'maxAge' => 0,
+];

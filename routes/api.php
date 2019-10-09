21f23a1235a4af070976ef7c1b5684abc8369089
Emmanuel Imoh <imohyo@gmail.com>
2019-10-09 02:19:45 +0000

    Users can edit profile and upload profile picture

--- routes/api.php
+++ routes/api.php
@@ -11,14 +11,29 @@
 |
  */
 
+// Route::routes();
+
+//
 Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
 
+//Get user profile
+Route::get('profile/{user}', 'ProfileController@index');
+
+//
 Route::group(['middleware' => 'auth:api'], function () {
+
+    //
     Route::resource('categories', 'CategoriesController');
+    //
     Route::resource('teams', 'TeamController');
     Route::post('teams/add-member', 'TeamController@addMember');
     Route::post('teams/remove-member', 'TeamController@removeMember');
     Route::get('teams/members/{id}', 'TeamController@viewMembers');
+    // Edit user profile
+    Route::post('profile/{user}/edit', 'ProfileController@update');
+
+   
+
 });
 

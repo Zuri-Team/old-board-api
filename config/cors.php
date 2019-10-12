3ffd6f60c09648841eb391c8645b6346cba86d79
Jude Jonathan <jonathanjude27@gmail.com>
2019-10-12 01:36:28 +0100

    Fixed allowwed headers

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
-    'supportsCredentials' => true,
+    'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
-    'allowedHeaders' => ['*'],
-    'allowedMethods' => ['GET', 'POST', 'PUT',  'DELETE'],
+    // 'allowedHeaders' => ['*'],
+    'allowedHeaders' => ['Accept', 'Content-Type', 'Origin', 'User-Agent', 'X-Requested-With', 'Authorization'],
+    'allowedMethods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     'exposedHeaders' => ['DAV', 'content-length', 'Allow'],
+    // 'exposedHeaders' => [],
     'maxAge' => 86400,
     'hosts' => [],
 ];

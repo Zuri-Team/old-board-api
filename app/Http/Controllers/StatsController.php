b817fbbad4bb6f7ca44493e4a4fd61a3eb7daf1b
Osueke Christian <osueke.christian@gmail.com>
2019-10-23 12:50:47 +0100

    Added statistical summary data for tracks, gender and stages

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -10,6 +10,9 @@ use Illuminate\Http\JsonResponse;
 use App\Team;
 use App\Post;
 use App\User;
+use App\Track;
+use App\TrackUser;
+use DB;
 
 class StatsController extends Controller
 {
@@ -31,5 +34,26 @@ class StatsController extends Controller
             return $this->sendSuccess($data, 'Statistics for Dashboard', 200);
         // }
         return $this->sendError('Internal server error.', 500, []);
+    }
+
+    public function summary(){
+        $id = 1;
+        $tracks = TrackUser::join('tracks', 'tracks.id', 'track_users.track_id')
+                        ->join('users', 'users.id', 'track_users.user_id')
+                        ->groupBy('tracks.track_name')
+                        ->select( 'tracks.track_name AS series', DB::raw('count(users.id) AS value') )
+                        ->get();
+        foreach($tracks as $track){ $track->id = $id; $track->group = 'Tracks'; $id+=1;}
+
+        $id = 1;
+        $genders = User::groupBy('gender')->select( DB::raw('count(id) AS value'), 'gender AS series' )->get();
+        foreach($genders as $gender){ $gender->id = $id; $gender->group = 'Gender'; $id+=1;}
+        
+        $id = 1;
+        $stages = User::groupBy('active', 'stage')->select( DB::raw('count(id) AS value'), 'stage AS group', 'active AS series' )->get();
+        foreach($stages as $track){ $track->id = $id; $track->group = 'Stage '.$track->group; $track->serires = $track->series == 1 ? 'Active' : 'Inactive'; $id+=1;}
+        
+
+        return $this->SUCCESS('Data Retrieved', ['stages'=>$stages, 'gender'=>$genders, 'tracks'=>$tracks]);
     }
 }

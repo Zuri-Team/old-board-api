db8f096ad1e9091a0fc0658169b69aede8983d1b
divergent_cix <tolu58@gmail.com>
2019-11-02 13:07:37 +0100

    Update TaskSubmission.php added the return keyword to the relationship

--- app/TaskSubmission.php
+++ app/TaskSubmission.php
@@ -10,11 +10,11 @@ class TaskSubmission extends Model
 
     public function tasks()
     {
-        $this->belongsTo('App/Task');
+        return $this->belongsTo('App/Task');
     }
     
     public function users()
     {
-        $this->hasMany('App/User');
+       return $this->belongsTo('App/User');
     }
 }

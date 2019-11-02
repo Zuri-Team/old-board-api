6acfee13fb20aa016f26c8dcbba3c57fe9cd68d5
divergent_cix <tolu58@gmail.com>
2019-11-02 18:37:43 +0100

    Update TaskSubmission.php Changed the relationship from hasMany to belongsTo

--- app/TaskSubmission.php
+++ app/TaskSubmission.php
@@ -13,8 +13,8 @@ class TaskSubmission extends Model
         return $this->belongsTo('App/Task');
     }
     
-    public function users()
+    public function user()
     {
-        returns $this->hasMany('App/User');
+        returns $this->belongsTo('App\User::class');
     }
 }

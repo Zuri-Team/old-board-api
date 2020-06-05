f86a25d26d6d00f1df8d312f2004077d4bf0cd93
Jude Jonathan <jonathanjude27@gmail.com>
2020-06-06 00:50:15 +0100

    push

--- app/Http/Controllers/TaskSubmissionController.php
+++ app/Http/Controllers/TaskSubmissionController.php
@@ -986,7 +986,7 @@ class TaskSubmissionController extends Controller
 
         foreach($data as $datum){
             $all_submissions++;
-            if($datum['status'] == 'pass' && !is_null($datum['email'])){
+            if($datum['status'] == 'pass' && !empty($datum['email'])){
                 $passed_submissions++;
 
                 $email = str_replace(' ', '', $datum['email']);

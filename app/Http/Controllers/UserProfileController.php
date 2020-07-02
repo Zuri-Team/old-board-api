7adb8965d2d0dcbc48fb1f09f4e094b3bc040157
KingAbesh <abeshekwere@gmail.com>
2020-07-02 13:32:22 +0100

    feat: bug fixes

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -481,11 +481,12 @@ class UserProfileController extends Controller
                 if (intval($stage) < 1 || intval($stage) > 10) {
                     continue;
                 } else {
-                    Slack::removeFromChannel($slack_id, $currentStage);
-                    Slack::addToChannel($slack_id, $stage);
                     $user->stage = $stage;
                     $count += 1;
-                    $user->save();
+                    if ($user->save()) {
+                        Slack::removeFromChannel($slack_id, $currentStage);
+                        Slack::addToChannel($slack_id, $stage);
+                    };
                 }
             }
         }
